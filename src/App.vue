cb30616dd7721f851cdc7dbff0d67c19bcfb94dd
nitslaszlo@gmail.com <nitslaszlo@gmail.com>
2018-08-29 19:48:41 +0200

    up

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,61 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <h1>Kereső</h1>
+    <!-- filterek -->
+    <div id="filters">
+        <input type="text" id="nameFilter" v-model="nameFilter">
+    </div>
+
+    <!-- találatok -->
+    <ol id="resultList">
+        <li v-for="resultItem in GetResults()" v-bind:key="resultItem">
+               {{ resultItem }}
+        </li>
+    </ol>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
 
-@Component({
-  components: {
-    HelloWorld
+@Component
+export default class App extends Vue {
+  nameFilter: string = "";
+  list: string[] = [];
+
+  private GetResults(): string[] {
+    return this.list.filter(item => {
+      if (this.nameFilter == "") return true; // ha nincs filter, akkor minden megjelenik
+      // pozíció megállapítása
+      let position = item.toLowerCase().indexOf(this.nameFilter.toLowerCase());
+      return position != -1;
+    });
   }
-})
-export default class App extends Vue {}
+
+  private created(): void {
+    this.list = [
+      "Get Out",
+      "Dunkirk",
+      "Lady Bird",
+      "Blade Runner 2049",
+      "A Ghost Story",
+      "The Florida Project",
+      "Phantom Thread",
+      "Call Me By Your Name",
+      "Raw",
+      "The Shape of Water",
+      "Okja",
+      "Personal Shopper",
+      "It Comes at Night",
+      "Good Time",
+      "Star Wars: The Last Jedi"
+    ];
+  }
+}
 </script>
 
 <style>
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
